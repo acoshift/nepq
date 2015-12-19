27533bdd3fe55b0bfb4830968add4b45bbf395ed
Thanatat Tamtan <acoshift@gmail.com>
2015-12-19 14:23:35 +0700

    update d.ts

--- src/nepq.d.ts
+++ src/nepq.d.ts
@@ -1,33 +1,35 @@
 declare module "nepq" {
 
-  function n(): Nq;
+  function n(): n.Nq;
 
-  interface NepQ {
-    method: string;
-    name: string;
-    params: any;
-    retrieves: any;
+  module n {
+    export interface NepQ {
+      method: string;
+      name: string;
+      params: any;
+      retrieves: any;
 
-    response(result?): any;
-  }
+      response(result?): any;
+    }
 
-  class Nq {
-    parser: {
-      parse(s: string),
-      on(event: string, handler: Function)
-    };
+    export class Nq {
+      parser: {
+        parse(s: string),
+        on(event: string, handler: Function)
+      };
 
-    constructor();
+      constructor();
 
-    parse(s: string, ...args): void;
+      parse(s: string, ...args): void;
 
-    bodyParser(): (req, res, next: Function) => void;
+      bodyParser(): (req, res, next: Function) => void;
 
-    on(method: string, name: string, callback: (q: NepQ, ...args) => void): void;
+      on(method: string, name: string, callback: (q: NepQ, ...args) => void): void;
 
-    use(callback: (q: NepQ, ...args) => void): void;
+      use(callback: (q: NepQ, ...args) => void): void;
 
-    error(callback: (...args) => void): void;
+      error(callback: (...args) => void): void;
+    }
   }
 
   export = n;
