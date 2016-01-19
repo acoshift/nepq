c9625ec2b645a70f276c2b608dc80ce7d2cf92ea
Thanatat Tamtan <acoshift@gmail.com>
2016-01-19 16:21:03 +0700

    edit test output

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ fs.readdirSync(path.join(__dirname, 'test'))
   .filter(function(x) { return x.substr(-3) === '.js'; })
   .map(function(x) { return x.substr(0, x.length - 3); })
   .forEach(function(x) {
-    it('parser: ' + x, function(cb) {
+    it(x, function(cb) {
       var t = require('./test/' + x);
       var n = nepq.parse(t.nepq);
       var o = t.obj;
