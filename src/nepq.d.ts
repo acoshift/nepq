5df3d6fe0484a288bf9c4a4127f2afd928c935f6
Thanatat Tamtan <acoshift@gmail.com>
2016-01-20 22:41:52 +0700

    update d.ts

--- src/nepq.d.ts
+++ src/nepq.d.ts
@@ -6,8 +6,10 @@ declare module "nepq" {
     retrieves: any;
   }
 
-  export function parser(): any;
-  export function parse(s: string): NepQ;
+  export var parser: {
+    parse: (input: string) => NepQ;
+  };
+  export function parse(input: string): NepQ;
   export function response(nq: NepQ, obj: any, cb?: (result: any, error: Error) => void): any;
   export function bodyParser(opt?: {
     encoding?: string;
