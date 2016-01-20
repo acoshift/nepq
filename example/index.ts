4c6cc5fa49a245c362d53d66bd5e67dd26441559
Thanatat Tamtan <acoshift@gmail.com>
2016-01-21 02:34:29 +0700

    update example

--- example/index.ts
+++ example/index.ts
@@ -24,21 +24,19 @@ app.use((req, res) => {
   switch (nq.method) {
     case 'create':
       if (!db[nq.name]) db[nq.name] = [];
-      nq.params._id = _id++;
-      db[nq.name].push(nq.params);
-      console.log(nepq.response(nq, nq.params));
-      response(nq.params);
+      nq.params[0]._id = _id++;
+      db[nq.name].push(nq.params[0]);
+      response(nq.params[0]);
       break;
     case 'read':
       if (!db[nq.name]) return response(null);
-      if (typeof nq.params === 'object' && Object.keys(nq.params).length === 0) {
+      if (nq.params.length === 0) {
         return response(db[nq.name]);
       }
-      response(db[nq.name].filter(x => x._id === nq.params)[0] || null);
+      response(db[nq.name].filter(x => x._id === nq.params[0])[0] || null);
       break;
     case 'update':
       if (!db[nq.name]) return response(null);
-      if (!(nq.params instanceof Array)) return response(null);
       i = get(nq.params[0]);
       if (i === null) return response(null);
       nq.params[1]._id = db[nq.name][i]._id;
@@ -47,7 +45,7 @@ app.use((req, res) => {
       break;
     case 'delete':
       if (!db[nq.name]) return response(null);
-      i = get(nq.params);
+      i = get(nq.params[0]);
       if (i === null) return response(null);
       d = db[nq.name][i];
       delete db[nq.name][i];
@@ -56,7 +54,6 @@ app.use((req, res) => {
     case 'calc':
       switch (nq.name) {
         case 'sum':
-          if (!(nq.params instanceof Array)) nq.params = [ nq.params ];
           response({
             result: ([init]) => nq.params.reduce((p, v) => p + v, init)
           });
