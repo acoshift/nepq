780af5c4c53a9697e595666c5d26545ddf6b01ed
Thanatat Tamtan <acoshift@gmail.com>
2016-01-21 16:10:03 +0700

    fix d.ts not a module

--- src/nepq.d.ts
+++ src/nepq.d.ts
@@ -1,17 +1,16 @@
-declare module "nepq" {
-  export interface NepQ {
-    method: string;
-    name: string;
-    params: any[];
-    retrieves: any;
-  }
-
-  export var parser: {
-    parse: (input: string) => NepQ;
-  };
-  export function parse(input: string): NepQ;
-  export function response(nq: NepQ, obj: any, cb?: (result: any, error: Error) => void): any;
-  export function bodyParser(opt?: {
-    encoding?: string;
-  }): (req, res, next) => void;
+export interface NepQ {
+  method: string;
+  name: string;
+  params: any[];
+  retrieves: any;
 }
+
+export var parser: {
+  parse: (input: string) => NepQ;
+};
+
+export function parse(input: string): NepQ;
+export function response(nq: NepQ, obj: any, cb?: (result: any, error: Error) => void): any;
+export function bodyParser(opt?: {
+  encoding?: string;
+}): (req, res, next) => void;
